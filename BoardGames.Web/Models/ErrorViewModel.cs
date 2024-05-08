7b547a78d165f5795a0f4ca653514ec4aab91d83
ryu2kk <ryu799@live.com.pt>
2024-05-08 17:00:42 +0100

    Add project files.

---
+++ BoardGames.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BoardGames.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
